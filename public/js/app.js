3e1cc6e02baa30f3807000459d950db513e9cf7d
rohan dahiya <rdahiya39@gmail.com>
2019-05-24 00:09:14 +0530

    Initial Commit

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+console.log('CLient side js file loaded')
+
+const weatherform = document.querySelector('form')
+const search = document.querySelector('input')
+const msg1 = document.querySelector('#msg1')
+const msg2 = document.querySelector('#msg2')
+
+weatherform.addEventListener('submit', (e) => {
+    e.preventDefault()
+    msg1.textContent='Loading...'
+    msg2.textContent=''
+    const location = search.value
+    fetch('http://localhost:3000/weather?address=' + encodeURIComponent(location)).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                msg1.textContent = data.error
+            }
+            else {
+                msg1.textContent = data.location
+                msg2.textContent = data.forecast
+            }
+        })
+    })
+})
---
+++ src/app.js
@@ -0,0 +1,83 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+app.use(express.static(path.join(__dirname, '../public')))
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Rohan'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About',
+        name: 'Rohan'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        name: 'Rohan'
+    })
+})
+
+const command = process.argv[2]
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'Please Enter a location!'
+        })
+    }
+    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+        if (error) {
+            return res.send({
+                error
+            })
+        }
+        forecast(latitude, longitude, (error, forecastdata) => {
+            if (error) {
+                return res.send({
+                    error
+                })
+            }
+            res.send({
+                forecast: forecastdata,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('error', {
+        title: '404',
+        name: 'Rohan',
+        errorMessage: 'Help article not found!'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('error', {
+        title: '404',
+        name: 'Rohan',
+        errorMessage: 'No page found!'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server Running.')
+})
