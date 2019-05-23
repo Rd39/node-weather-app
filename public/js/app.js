9877aa652118936366077a18d082437306f28d66
rohan dahiya <rdahiya39@gmail.com>
2019-05-24 00:11:32 +0530

    Remove unnecessary console.log calls

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('CLient side js file loaded')
-
 const weatherform = document.querySelector('form')
 const search = document.querySelector('input')
 const msg1 = document.querySelector('#msg1')
